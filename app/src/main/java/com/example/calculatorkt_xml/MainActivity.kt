4193c303e7ae69efa344098a04df7c590af76ed4
nishant <nishantpriya.scorpion@gmail.com>
2025-12-21 16:55:04 +0530

    layout

--- app/src/main/java/com/example/calculatorkt_xml/MainActivity.kt
+++ app/src/main/java/com/example/calculatorkt_xml/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.calculatorkt_xml
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun onDigit(view: View){
+         Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
     }
 }
